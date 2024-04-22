35cecc26f6353fdbedbc4ab772189d781cc44c3c
IT0198 <Konankidshanda@icsc.com.tw>
2024-04-22 17:24:39 +0800

    section10 Chapter5 107~109

---
+++ 1.1 JS/app.js
@@ -0,0 +1,13 @@
+// console.log("Hello World!");
+// window.alert("Welcome!");
+
+// let user_name = window.prompt("請輸入你的名字：");
+// window.alert("Welcome " + user_name + " !");
+
+let x; // declare a variable
+x = 10; // variable assignment
+// 可將以上兩者整合寫為 let x = 10;
+
+const y = 50;
+
+console.log(x); //看x的值為多少
